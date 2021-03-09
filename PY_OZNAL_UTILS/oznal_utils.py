8c9b4a170d53dbb05b34f0f672ebbb597f691704
ramang22 <wibrach@gmail.com>
2021-03-09 10:56:30 +0100

    setup oznal

---
+++ PY_OZNAL_UTILS/oznal_utils.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+import requests
+import io
+import yfinance as yf
+import datetime
+import time
+
+class OZNAL_UTIL:
+
+    def downloadNASDAQHistoricalStockData(self, symbol, start, end):
+        stock_final = pd.DataFrame()
+        try:
+            stock = []
+            stock = yf.download(symbol,start=start, end=end, progress=False)
+            
+            if len(stock) == 0:
+                None
+            else:
+                stock['Name']=symbol
+                stock_final = stock_final.append(stock,sort=False)
+        except Exception:
+            None
+        return stock_final
+
+    def getStockSymbols(self, onlySymbols=True):
+        url="https://pkgstore.datahub.io/core/nasdaq-listings/nasdaq-listed_csv/data/7665719fb51081ba0bd834fde71ce822/nasdaq-listed_csv.csv"
+        s = requests.get(url).content
+        companies = pd.read_csv(io.StringIO(s.decode('utf-8')))
+        if onlySymbols:
+            return companies['Symbol'].tolist()
+        else:
+            return companies    
+
+
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # OZNAL-StockPrediction
+
+
+## Zdroje
+
+* https://towardsdatascience.com/downloading-historical-stock-prices-in-python-93f85f059c1f
+* https://www.kaggle.com/unanimad/reddit-rwallstreetbets
+* https://www.kaggle.com/mattpodolak/rwallstreetbets-posts-and-comments?select=wallstreetbets_posts.csv
+* https://www.kaggle.com/gpreda/reddit-wallstreetsbets-posts
