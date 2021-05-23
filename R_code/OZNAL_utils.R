dd9ebefec839656c25a43119bf2c39f96e648442
ramang22 <wibrach@gmail.com>
2021-05-23 20:54:29 +0200

    bordel pre tomasa

--- R_code/OZNAL_utils.R
+++ R_code/OZNAL_utils.R
@@ -2,3 +2,28 @@
 test <- function (){
   print("kappa")
 }
+
+head(df)
+
+getBestValue <- function(){
+  df <- read.csv("data/dataset_final.csv")
+  df <- df[,-2]
+  repre <- df[df$symbol == 'AAPL',]
+}
+
+customBoxPlot <-function(){
+  boxplot(df$score)
+}
+
+customBoxPlot()
+best <- getBestValue()
+head(best)
+# 
+# 2. Q-Q Plot
+# 3. Boxplot - vsetky
+# 4. Histogram - vsetky
+# 5. Graf odhadu hustoty
+# 6. Graf spicatosti
+# P-Q plot
+# Graf rozptylu 
+# stock graf - svieckovy - vsetky
